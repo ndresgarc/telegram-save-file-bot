d603813996bc8a8c61410c39c3b4720eb8e5fd85
ndresgarc <email@andresgarcia.dev>
2023-11-25 00:24:37 +0100

    feat: create utils barrel

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+import getHandler from './get-handler.cjs';
+import getUsersIds from './get-users-ids';
+import isAllowedUser from './is-allowed-user';
+import loadConfig from './load-config';
+
+export const getHandler = getHandler;
+export const getUsersIds = getUsersIds;
+export const isAllowedUser = isAllowedUser;
+export const loadConfig = loadConfig;
