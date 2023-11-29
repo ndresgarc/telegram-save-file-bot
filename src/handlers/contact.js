b1f67f2c2646d542cefb293e258ec7ce4df1e677
ndresgarc <email@andresgarcia.dev>
2023-11-29 22:40:47 +0100

    feat: add contact handling

--- src/handlers/contact.js
+++ src/handlers/contact.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+
 export const contact = (msg, bot, config) => {
     console.log('contact handler');
+
+    let fileName = `${msg.contact.first_name} ${msg.contact.last_name}`;
+
+    fs.writeFile(`${config.saveFolder}${fileName}.vcard`, msg.contact.vcard, (err) => { 
+        if (err) 
+          console.log(err); 
+        else { 
+          console.log("File written successfully\n"); 
+        } 
+      }); 
+
 };
