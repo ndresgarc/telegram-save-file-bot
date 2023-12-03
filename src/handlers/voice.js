5b02c97752920e9e0984bc95d745f8219e5b8362
ndresgarc <email@andresgarcia.dev>
2023-12-03 23:07:16 +0100

    chore: TODO voice

--- src/handlers/voice.js
+++ src/handlers/voice.js
@@ -4,4 +4,7 @@ export const voice = (msg, bot, config) => {
     bot.downloadFile(voiceId, config.saveFolder).then(()=>{
         bot.sendMessage(msg.chat.id, 'Done!');
     });
+
+    // TODO: Convert voice to text and save it to a file
+
 };
