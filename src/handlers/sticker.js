e30e00a2202861168800e55ce8dc332ef4b41d1f
ndresgarc <email@andresgarcia.dev>
2023-12-11 23:48:35 +0100

    feat: download sticker

--- src/handlers/sticker.js
+++ src/handlers/sticker.js
@@ -2,8 +2,17 @@ export const sticker = (msg, bot, config) => {
     console.log('sticker handler');
     bot.sendMessage(msg.chat.id, 'Procesing...');
     var sticker = msg.sticker;
+
+    bot.downloadFile(sticker.file_id, './stickers').then((path) => {
+        console.log(path);
+        bot.sendMessage(msg.chat.id, 'Done!');
+    });
+
+    /* Add as image metadata */
+    /*
     bot.getFileLink(sticker.file_id).then((link) => {
         console.log(link);
         bot.sendMessage(msg.chat.id, 'Done!');
     });
+    */
 }
